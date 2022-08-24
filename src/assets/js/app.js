e66dd4d5fc007a8aa049253da7db6f0d9c066511
KonstantinVolodko <volodko2000@bk.ru>
2022-08-24 17:51:16 +0300

    adaptiv

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -20,7 +20,7 @@ let swiper = new Swiper(".mySwiper", {
 
 
 let productsSlider = new Swiper(".productsSlider", {
-    slidesPerView: 3,
+    slidesPerView: 1,
     loop: true,
     pagination: {
         el: ".swiper-pagination2",
@@ -31,7 +31,57 @@ let productsSlider = new Swiper(".productsSlider", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+
+    breakpoints: {
+        // when window width is >= 320px
+        1124: {
+            slidesPerView: 3,
+        },
+
+        745: {
+            slidesPerView: 2,
+        },
+    }
 }) 
+
+
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+  
+function enableScroll() {
+    window.onscroll = function() {};
+}
+
+let menuBtn = document.querySelector('.menuBtn')
+let menu = document.querySelector('.menu')
+let overlay = document.querySelector('.overlay')
+let cross = document.querySelector('.cross')
+
+menuBtn.addEventListener('click', e => {
+    menu.classList.toggle('activeMenu')
+    overlay.classList.toggle('overlayActive')
+    disableScroll()
+})
+
+overlay.addEventListener('click', e => {
+    menu.classList.toggle('activeMenu')
+    overlay.classList.toggle('overlayActive')
+    enableScroll()
+})
+
+cross.addEventListener('click', e => {
+    menu.classList.toggle('activeMenu')
+    overlay.classList.toggle('overlayActive')
+    enableScroll()
+})
 
 })
 
